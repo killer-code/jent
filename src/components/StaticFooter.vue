02016cdd427206bf78dcd4a022c846cf8085ed1e
Sergey Gorshkov <gorshkov88@gmail.com>
2020-11-22 14:58:10 +0400

    add footer for static pages

---
+++ src/components/StaticFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <p>
+      Владелец регистрационного удостоверения/Организация, принимающая претензии
+      от потребителей
+    </p>
+    <p>ООО «Фармамед», тел. (812) 647 02 46, сайт: JENT.MEN</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'StaticFooter',
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  p {
+    color: white;
+    opacity: 0.5;
+  }
+}
+</style>
