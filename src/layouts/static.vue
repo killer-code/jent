a2efb59b176bcadaacbf56141f96f1330e3f5e94
Sergey Gorshkov <gorshkov88@gmail.com>
2020-11-22 16:48:09 +0400

    add layout for static pages

---
+++ src/layouts/static.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="wrapper-page">
+    <Header :asideData="asideData" />
+    <main class="app-page">
+      <router-view></router-view>
+    </main>
+  </section>
+</template>
+
+<script>
+import Header from '@/components/Header';
+
+export default {
+  name: 'default',
+  components: { Header },
+  data: () => ({
+    asideData: {
+      isOpen: false,
+    },
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
+.app-page {
+  position: relative;
+  margin: 200px auto 50px;
+  max-width: 1200px;
+  padding: 0 30px;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  min-height: calc(100vh - 250px);
+}
+.wrapper-page {
+  background: #0a0b11;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
