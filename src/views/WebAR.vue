9ebafa33d1baa26777c2ee52ba850889928123f8
Sergey Gorshkov <gorshkov88@gmail.com>
2020-11-22 17:13:03 +0400

    add mobile view

--- src/views/WebAR.vue
+++ src/views/WebAR.vue
@@ -6,7 +6,7 @@
       в дополненной реальности, используя мобильный браузер.
     </div>
 
-    <div class="steps">
+    <div class="steps steps--desktop">
       <img src="@/assets/img/webar-qr.svg" alt="qr code" class="steps__qr" />
       <div class="steps__items">
         <div class="step">
@@ -28,6 +28,29 @@
         <div class="step">
           <div class="step__title">
             3. Наслаждайтесь дополненной реальностью!
+          </div>
+          <div class="step__description">
+            Правила игры очень простые - распыляйте препарат на предметы.
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="steps steps--mobile">
+      <div class="steps__items">
+        <div class="step">
+          <div class="step__title">1. Пройдите по ссылке</div>
+          <a
+            class="step__link"
+            href="https://medium.com/phygitalism/web-ar-348de5d7188"
+          >
+            Web AR
+          </a>
+        </div>
+
+        <div class="step">
+          <div class="step__title">
+            2. Наслаждайтесь дополненной реальностью!
           </div>
           <div class="step__description">
             Правила игры очень простые - распыляйте препарат на предметы.
@@ -60,24 +83,51 @@ export default {
     font-size: 42px;
     line-height: 130%;
     margin-bottom: 36px;
+
+    @media screen and (max-width: 1280px) {
+      font-size: 36px;
+      line-height: 130%;
+      margin-bottom: 32px;
+    }
+
+    @media screen and (max-width: 560px) {
+      font-size: 26px;
+      line-height: 130%;
+    }
   }
 
   &__about {
     font-size: 24px;
     line-height: 140%;
     margin-bottom: 60px;
+
+    @media screen and (max-width: 560px) {
+      font-size: 16px;
+      line-height: 130%;
+      margin-bottom: 40px;
+    }
   }
 
   .steps {
     display: flex;
 
+    &--desktop {
+      @media screen and (max-width: 1280px) {
+        display: none;
+      }
+    }
+
+    &--mobile {
+      display: none;
+      @media screen and (max-width: 1280px) {
+        display: flex;
+      }
+    }
+
     &__qr {
       width: 202px;
       height: 202px;
       margin-right: 87px;
-    }
-
-    &__items {
     }
   }
 
@@ -88,11 +138,14 @@ export default {
       font-size: 21px;
       line-height: 100%;
       margin-bottom: 24px;
+      text-indent: -22px;
+      margin-left: 22px;
     }
 
     &__description {
       font-size: 18px;
       line-height: 150%;
+      padding-left: 22px;
 
       a {
         color: inherit;
@@ -101,6 +154,17 @@ export default {
           color: #f36d01;
         }
       }
+    }
+
+    &__link {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: white;
+      width: 220px;
+      padding: 13px 0;
+      border: 1px solid #f36d01;
+      border-radius: 8px;
     }
   }
 }
